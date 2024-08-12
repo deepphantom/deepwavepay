ee936ec3f296c9ba13be627993e33e667e249a17
Oludowole Olumide <xeonncodes@gmail.com>
2024-08-12 04:00:22 +0100

    Updated

--- layouts/onboard/form.tsx
+++ layouts/onboard/form.tsx
@@ -11,14 +11,15 @@ export const FormLayout = ({title, subtitle, body} : {title: string, subtitle: s
 
                 <div className="w-[93%] sm:w-[500px]">
 
-                <Card className="py-20 px-5 sm:px-8 w-full">
+                <Card className="py-10 sm:py-20 px-5 sm:px-8 w-full">
 
                     {/* Heading */}
                     <div className="mb-1 flex-col flex gap-1">
                     <p className={onboardtitle({size: 'sm'})}>{title}</p>
                     <p className={`${onboardSubtitle()} font-light`}>{subtitle}</p>
                     </div>
-
+                    
+                    {/* Divider */}
                     <Divider />
                     
                     {/* Body */}
