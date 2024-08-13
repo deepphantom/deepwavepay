c3c80a94c88559ffb5ad8a6076564f287210e6c2
Oludowole Olumide <xeonncodes@gmail.com>
2024-08-13 06:14:25 +0100

    Updated

---
+++ config/images.ts
@@ -0,0 +1,3 @@
+export const siteImagesPath = {
+    appLogo: require('../public/assets/logo.png')
+}
--- layouts/onboard/form.tsx
+++ layouts/onboard/form.tsx
@@ -7,6 +7,7 @@ import { AppLogo } from "@/components/Icon"
 import Link from "next/link"
 import { siteConfig } from "@/config/site"
 import Image from "next/image"
+import { siteImagesPath } from "@/config/images"
 
 export const FormLayout = ({title, subtitle, children} : {title: string, subtitle: string, children:ReactNode } )=> {
     
@@ -15,19 +16,25 @@ export const FormLayout = ({title, subtitle, children} : {title: string, subtitl
             <section className="flex h-[100%] items-start">
 
                 {/* Left */}
-                <div className="basis-[100%] flex flex-col p-10 md:basis-[50%] h-[100%]">
+                <div className="basis-[100%] flex flex-col p-7 sm:p-10 md:basis-[50%] h-[100%]">
 
                     {/* Heading */}
                     <div className="flex items-center justify-between">
                         {/* Logo */}
-                        <AppLogo />
+                        <Link href="/">
+                            <Image 
+                                src={siteImagesPath.appLogo}
+                                style={{width: 130, objectFit: 'cover' }}
+                                alt="sitelogo"
+                            />
+                        </Link>
                         {/* Link */}
                         <Link className="text-[13px] font-bold text-md text-black underline" href={'/'}>Create an account</Link>
                     </div>
 
                     {/* Body */}
                     <div className="flex flex-1 items-center justify-center">
-                        <div className="w-[350px] flex flex-col items-center">
+                        <div className="w-[95%] sm:w-[350px] flex flex-col items-center">
                             {children}
                         </div>
                     </div>
